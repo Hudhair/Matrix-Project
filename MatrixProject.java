563d726fcb04bfa6e04733326c1c49579922844c
asahota1 <sahotaa679@gmail.com>
2024-05-13 16:52:54 -0500

    Update MatrixProject.java

--- MatrixProject.java
+++ MatrixProject.java
@@ -70,7 +70,6 @@ public class MatrixProject {
         System.out.println();
         
         //THIS SECTION PRINTS MATRIX 2
-            //Laa
         for(int i = 0; i < matrix2.length; i++){
             for(int j = 0;j < matrix2[i].length; j++){
                 System.out.print(matrix2[i][j]);
