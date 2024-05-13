15f9825563ccd3fc96bca6d940c0ac55bfb56e7c
asahota1 <158601049+asahota1@users.noreply.github.com>
2024-05-13 15:30:56 -0500

    Update MatrixProject.java
    checking to see if i can change things

--- MatrixProject.java
+++ MatrixProject.java
@@ -70,6 +70,7 @@ public class MatrixProject {
         System.out.println();
         
         //THIS SECTION PRINTS MATRIX 2
+            //Laa
         for(int i = 0; i < matrix2.length; i++){
             for(int j = 0;j < matrix2[i].length; j++){
                 System.out.print(matrix2[i][j]);
