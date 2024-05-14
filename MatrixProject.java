6f762fcbbd349c2a82225bd6c04b2cd9f0987e31
asahota1 <sahotaa679@gmail.com>
2024-05-14 16:35:31 -0500

    added formatting to output
    ;)

--- MatrixProject.java
+++ MatrixProject.java
@@ -64,19 +64,22 @@ public class MatrixProject {
         System.out.println();
         
         //THIS SECTION PRINTS MATRIX 1
-        System.out.println("_");
-        for(int i = 0; i < matrix1.length; i++){
-            for(int j = 0;j < matrix1[i].length; j++){
-                if(i=0)
-                    System.out.println("| ");
-                System.out.print(matrix1[i][j]);
-                if(j=matrix1[i].length)
-                    System.out.println(" |");
+        System.out.println("Matrix 1: ");
+        for(int r = 0; r < matrix1.length; r++){
+            for(int c = 0; c < matrix1[r].length; c++){
+                if(c==0)
+                    System.out.print("[ ");
+                System.out.print(matrix1[r][c]);
+                if(c!=matrix1[r].length-1){
+                    System.out.print(", ");
+                }
+                if(c==matrix1[r].length-1)
+                    System.out.print(" ]");
             }
             System.out.println();
         }
-        System.out.println("-");
-        
+
+
         System.out.println();
         
         //THIS ENTIRE SECTION TAKES IN VALUES FOR MATRIX 2
@@ -95,9 +98,17 @@ public class MatrixProject {
         System.out.println();
         
         //THIS SECTION PRINTS MATRIX 2
-        for(int i = 0; i < matrix2.length; i++){
-            for(int j = 0;j < matrix2[i].length; j++){
-                System.out.print(matrix2[i][j]);
+        System.out.println("Matrix 2: ");
+        for(int r = 0; r < matrix2.length; r++){
+            for(int c = 0; c < matrix2[r].length; c++){
+                if(c==0)
+                    System.out.print("[ ");
+                System.out.print(matrix2[r][c]);
+                if(c!=matrix2[r].length-1){
+                    System.out.print(", ");
+                }
+                if(c==matrix2[r].length-1)
+                    System.out.print(" ]");
             }
             System.out.println();
         }
@@ -105,13 +116,22 @@ public class MatrixProject {
 
         //THIS SECTION CREATES THE PRODUCT MATRIX AND PRINTS IT
         int[][] product = MatrixMultiplier(matrix1,matrix2);
-            for(int i =0; i < product.length; i++){
-                for(int j=0;j<product[i].length;j++){
-                    System.out.print(product[i][j]);
+        
+        System.out.println("Product Matrix: ");
+        for(int r = 0; r < product.length; r++){
+            for(int c = 0; c < product[r].length; c++){
+                if(c==0)
+                    System.out.print("[ ");
+                System.out.print(product[r][c]);
+                if(c!=product[r].length-1){
+                    System.out.print(", ");
                 }
-                System.out.println();
-            
+                if(c==product[r].length-1)
+                    System.out.print(" ]");
+            }
+            System.out.println();
         }
+            
         input.close();
     }
 
