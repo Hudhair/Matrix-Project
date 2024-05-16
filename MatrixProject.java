89628bfc22b2d3f25e0a9b16640b22daffcdd4db
HudhairA <148573517+HudhairA@users.noreply.github.com>
2024-05-15 20:13:37 -0500

    Fixed bug when column 1 =1 and row 2 =1

--- MatrixProject.java
+++ MatrixProject.java
@@ -44,22 +44,8 @@ public class MatrixProject {
         int[][] matrix2 = new int[row2][column2];
         
         System.out.println();
-        
+
         //THIS ENTIRE SECTION TAKES IN VALUES FOR MATRIX 1
-        //removing variables and using for loops; row then column
-        /*
-         *  int rowcounterforstring = 1;
-         *  int columncounterforstring = 1;
-         *  for(int i = 0; i < matrix1.length; i++){
-                for(int j = 0;j < matrix1[i].length; j++){
-                    System.out.print("Enter the value in row "+rowcounterforstring+" and column "+columncounterforstring+" for the first matrix: ");
-                    matrix1[i][j] = input.nextInt();
-                    columncounterforstring++;
-                }
-            columncounterforstring=1;
-            rowcounterforstring++;
-            }
-         */
         for(int r = 0; r < matrix1.length; r++){
             for(int c = 0;c < matrix1[r].length; c++){
                 System.out.print("Enter the value in row "+(r+1)+" and column "+(c+1)+" for the first matrix: ");
@@ -141,7 +127,8 @@ public class MatrixProject {
          
         for (int i = 0; i < matrix1.length; i++) {
             for (int j = 0; j <matrix2[0].length; j++) {
-                for (int k = 0; k < matrix1.length; k++) {
+                product[i][j]=0;
+                for (int k = 0; k < matrix1[0].length; k++) {
                     product[i][j] += matrix1[i][k] * matrix2[k][j];
                 }
             }
