9fd92976e40b695f1a8a1b0dfb3e88b064f5a196
asahota1 <sahotaa679@gmail.com>
2024-05-20 17:16:28 -0500

    Added doubles and made matrix addition work
    Needs to format the doubles to ensure its aight...

--- MatrixProject.java
+++ MatrixProject.java
@@ -12,6 +12,18 @@ public class MatrixProject {
         int column2 = 0;
         Scanner input = new Scanner(System.in);
         
+        //Ask if matrix addition and multiplcation:
+        System.out.println("Would you like to add matrices or multiply them.");
+        System.out.println("Type 1 for Multiplcation or Type 2 for Addition");
+        int addOrMul = input.nextInt();
+
+        //Checker to ensure 1 or 2 has been entered
+        while (addOrMul != 1 && addOrMul !=2 ){
+            System.out.println("We can only add or multiply...");
+            System.out.println("Type 1 for Multiplcation or Type 2 for Addition");
+            addOrMul = input.nextInt();
+        }
+
         //TAKES IN ROWS AND THEN COLUMNS FOR MATRIX 1
         System.out.print("How many rows are in your first matrix? ");
         row1 = input.nextInt();
@@ -24,8 +36,10 @@ public class MatrixProject {
         System.out.print("How many columns are in your second matrix? ");
         column2 = input.nextInt();
         
-        while(column1 != row2){
-                System.out.println("These matrices cannot be multiplied. Re-enter another matrix: ");
+        //For matrix multiplcation check and ask if to instead add
+        if(addOrMul == 1){
+            while(column1 != row2){
+                System.out.println("These matrices cannot be multiplied. Re-enter another matrices: ");
 
                 //TAKES IN ROWS AND THEN COLUMNS FOR MATRIX 1
                 System.out.print("How many rows are in your first matrix? ");
@@ -39,9 +53,28 @@ public class MatrixProject {
                 System.out.print("How many columns are in your second matrix? ");
                 column2 = input.nextInt();                
             }
+        } 
+        //for matrix multiplcation: ensures that we can add and also asks to multiply instead
+        if(addOrMul == 2){
+            while(column1 != column2 || row1 != row2){
+                System.out.println("These matrices cannot be added. Re-type other matrices: ");
 
-        int[][] matrix1 = new int[row1][column1];
-        int[][] matrix2 = new int[row2][column2];
+                //TAKES IN ROWS AND THEN COLUMNS FOR MATRIX 1
+                System.out.print("How many rows are in your first matrix? ");
+                row1 = input.nextInt();
+                System.out.print("How many columns are in your first matrix? ");
+                column1 = input.nextInt();
+
+                //TAKES IN ROWS AND THEN COLUMNS FOR MATRIX 2
+                System.out.print("How many rows are in your second matrix? ");
+                row2 = input.nextInt();
+                System.out.print("How many columns are in your second matrix? ");
+                column2 = input.nextInt();  
+            }
+        }
+
+        double[][] matrix1 = new double[row1][column1];
+        double[][] matrix2 = new double[row2][column2];
         
         System.out.println();
 
@@ -49,7 +82,7 @@ public class MatrixProject {
         for(int r = 0; r < matrix1.length; r++){
             for(int c = 0;c < matrix1[r].length; c++){
                 System.out.print("Enter the value in row "+(r+1)+" and column "+(c+1)+" for the first matrix: ");
-                matrix1[r][c] = input.nextInt();
+                matrix1[r][c] = input.nextDouble();
             }
         }
         
@@ -78,7 +111,7 @@ public class MatrixProject {
         for(int r = 0; r < matrix2.length; r++){
             for(int c = 0;c < matrix2[r].length; c++){
                 System.out.print("Enter the value in row "+(r+1)+" and column "+(c+1)+" for the second matrix: ");
-                matrix2[r][c] = input.nextInt();
+                matrix2[r][c] = input.nextDouble();
             }
         }
         
@@ -101,29 +134,51 @@ public class MatrixProject {
         }
         System.out.println();
 
-        //THIS SECTION CREATES THE PRODUCT MATRIX AND PRINTS IT
-        int[][] product = MatrixMultiplier(matrix1,matrix2);
-        
-        System.out.println("Product Matrix: ");
-        for(int r = 0; r < product.length; r++){
-            for(int c = 0; c < product[r].length; c++){
-                if(c==0)
-                    System.out.print("[ ");
-                System.out.print(product[r][c]);
-                if(c!=product[r].length-1){
-                    System.out.print(", ");
-                }
-                if(c==product[r].length-1)
-                    System.out.print(" ]");
-            }
-            System.out.println();
-        }
+        //THIS SECTION CREATES THE PRODUCT/SUM MATRIX AND PRINTS IT
+
+        if(addOrMul == 1) {
+            double [][]product = MatrixMultiplier(matrix1, matrix2);
             
+            System.out.println("Product Matrix: ");
+            for(int r = 0; r < product.length; r++){
+                for(int c = 0; c < product[r].length; c++){
+                    if(c==0)
+                        System.out.print("[ ");
+                    System.out.print(product[r][c]);
+                    if(c!=product[r].length-1){
+                        System.out.print(", ");
+                    }
+                    if(c==product[r].length-1)
+                        System.out.print(" ]");
+                }
+                System.out.println();
+            }
+        }
+
+        if(addOrMul == 2){
+            double[][] sum = MatrixAddition(matrix1,matrix2);
+
+            System.out.println("Sum Matrix: ");
+            for(int r = 0; r < sum.length; r++){
+                for(int c = 0; c < sum[r].length; c++){
+                    if(c==0)
+                        System.out.print("[ ");
+                    System.out.print(sum[r][c]);
+                    if(c!=sum[r].length-1){
+                        System.out.print(", ");
+                    }
+                    if(c==sum[r].length-1)
+                        System.out.print(" ]");
+                }
+                System.out.println();
+            }     
+        } 
+
         input.close();
     }
 
-    public static int[][] MatrixMultiplier(int[][] matrix1, int[][] matrix2){
-        int[][] product = new int[matrix1.length][matrix2[0].length];
+    public static double[][] MatrixMultiplier(double[][] matrix1, double[][] matrix2){
+        double[][] product = new double[matrix1.length][matrix2[0].length];
          
         for (int i = 0; i < matrix1.length; i++) {
             for (int j = 0; j <matrix2[0].length; j++) {
@@ -137,8 +192,8 @@ public class MatrixProject {
     }
 
     //I have not tested to see if this works
-    public static int[][] MatrixAddition(int[][] matrix1, int[][] matrix2){ 
-        int[][] sum = new int[martix1.length][matrix2[0].length];
+    public static double[][] MatrixAddition(double[][] matrix1, double[][] matrix2){ 
+        double[][] sum = new double[matrix1.length][matrix2[0].length];
 
         for(int row = 0; row < matrix1.length; row++){
             for(int col = 0; col < matrix2[0].length; col++){
